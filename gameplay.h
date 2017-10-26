53ddc131c7181de35f194fe756377d65d5328f68
Davide Faconti <davide.faconti@gmail.com>
2017-10-26 19:33:47 +0200

    class Player added

--- gameplay.h
+++ gameplay.h
@@ -3,11 +3,32 @@
 
 #include "base_types.h"
 
+class GameEngine;
+
 // any player has a set of Pieces. Whn the size is zero, the player wins
-typedef std::vector<Piece> PiecesOwned;
+class Player{
+public:
+    Player(): _first_turn(true) {}
+
+    const std::vector<Piece>& ownedPieces() const { return _owned_pieces; }
+
+    std::vector<Piece>& ownedPieces()             { return _owned_pieces; }
+
+    bool isFirstTurn()                      const { return _first_turn; }
+private:
+
+    std::vector<Piece> _owned_pieces;
+    // the first time the player play, it must
+    // do at least 30 points and can not attach to anything until
+    // he did.
+    bool _first_turn;
+
+    friend class GameEngine;
+};
+
 
 // This structure should store the entire state of the game.
-// Should be sufficient to Load and Save the entire state of the game to file.
+// It should be sufficient to Load and Save the entire state of the game to file.
 
 struct GameState{
     // combination already dropped on the table
@@ -17,9 +38,7 @@ struct GameState{
     RandomPiecesStack piece_stack;
 
     // each player has its own set of pieces
-    std::vector<PiecesOwned> pieces_by_player;
-
-    size_t numPlayers() const { return pieces_by_player.size(); }
+    std::vector<Player> players;
 };
 
 
@@ -28,6 +47,10 @@ bool SaveToFile(const GameState& source_state,const char* filename);
 bool LoadFromFile(const char* filename, GameState& destination_state);
 
 
+// I propose to not store GameState in GameEngine.
+// at least for now. It might make it easier to tru different strategies in parallel
+// using multithread.
+// Any thread should have its own copy of GameState to avoid concurrency problems.
 class GameEngine{
 public:
     GameEngine(int num_players);
@@ -44,6 +67,7 @@ public:
      * @return                 true if the player did something and the state was changed.
      */
     bool play(int player_number, GameState& state);
+
 };
 
 #endif // GAMEPLAY_H

904422f622900d3192ac243cb2fb5ac9d55ac5f8
Marcel Soler Ferret <marcelsolerferret@gmail.com>
2017-12-06 18:03:26 +0100

    - Implemented playNumericalCombWithJolly.

--- gameplay.cpp
+++ gameplay.cpp
@@ -251,29 +251,36 @@ const int combFirstPiece,const int numPiecesTofirst, int firstPieceCounter,const
 /* TODO implement this
 **************************************************************************
 * combFirstPiece     *
-*                    1 3 5 5 5 7 10 13 ...         playerHand
-* firstValidPiece        *
-* lastValidPiece                   *
+*                    1 5 5 5 7 10 13 ...         playerHand
+* firstValidPiece      *
+* lastValidPiece             *
 *
-* numberOfPiecesToValid  3
-* firstPieceCounter          *
-*                    0 1 1 0 3 0 1 0 0       counter
+* numPiecesTofirst  1
+* numPiecesToLast   4
+* firstPieceCounter         *
+*                     1 0 0 3 15 1 0 0       counter
+* jolly                        *
 *
-*
-* (firstValidPiece = combFirstPiece + numberOfPiecesToValid)
 ****************************************************************************
 */
 void selectValidCombFromCounterJolly(const std::vector<Piece>& playerHand, PieceCombination& validComb, const std::array<uint,13>& counter,
                 const int combFirstPiece, const int numPiecesTofirst, int firstPieceCounter, const int numPiecesToLast, const int jollyPieces)
 {
     int firstValidPiece = combFirstPiece + numPiecesTofirst;
-    for(int i=firstValidPiece; i< (combFirstPiece + numPiecesToLast); i++)
+    for(int i=firstValidPiece; i < (combFirstPiece + numPiecesToLast); i++)
     {
-        assert(counter[firstPieceCounter] > 0); validComb.push_back(playerHand[i]);
-        if(counter[firstPieceCounter]>1)
+        if(counter[firstPieceCounter] == 15)
+        {
+             validComb.push_back(Jolly);
+             i--; // not a playerHand piece
+        }
+        else{ validComb.push_back(playerHand[i]); }
+
+        if(counter[firstPieceCounter] > 1 && counter[firstPieceCounter] != 15 )
         {
             i+= (counter[firstPieceCounter] - 1); // skip repeated pieces go to next valid
         }
+
         firstPieceCounter++;
     }
 }
@@ -312,7 +319,7 @@ void dropNumericalCombinations( std::vector<Piece>& playerHand, std::vector<Piec
         {
             if(counter[j]==0 || j == 12)
             {
-                if( (last - firstValid) >= 3)
+                if( (last - firstValid) >= 2)
                 {
                     selectValidCombFromCounter(playerHand, validComb, counter, combFirstPiece, numPiecesTofirst, firstValid, numPiecesToLast);
                     assert(validComb.isValidCombination()); validCombinations.push_back(validComb);
@@ -437,35 +444,55 @@ void dropJollyNumbericalCombinations( std::vector<Piece>& playerHand, std::vecto
         int firstValid=0;
         int last = 0;
         int numPiecesToLast = 0;
+        bool isValidComb=false;
         for(int j=0; j<13; j++)
         {
-            if(counter[j]==0 || j == 12)
-            {
-                if( (last - firstValid) >= ( 3 - jollyPieces))
+
+                if(counter[j]==0 || j == 12)
                 {
-                    selectValidCombFromCounterJolly(playerHand, validComb, counter, combFirstPiece, numPiecesTofirst, firstValid, numPiecesToLast, jollyPieces);
-                    while( validComb.size() < 3)
+                    if(j==12) // corner case last number is valid
                     {
-                        validComb.push_back(Jolly);
+                        if(counter[j]>0){
+                            if(firstValid!=-1){last=j;}
+                        }
                     }
-                    assert(validComb.isValidCombination()); validCombinations.push_back(validComb);
-                    validComb.clear();
+                    if(firstValid!=-1 && last == -1 && counter[j+1] > 0 )  // counter= 0 1 0 1 0 0 ...
+                    {                                                      //              *
+                        counter[j]=15; // is not possible to have 15 times the same piece, use 15 to identify a jolly.
+                    }
+                    else if((last - firstValid) >= 1)
+                    {
+                        selectValidCombFromCounterJolly(playerHand, validComb, counter, combFirstPiece, numPiecesTofirst, firstValid, numPiecesToLast, jollyPieces);
+                        while( validComb.size() < 3)
+                        {
+                          validComb.push_back(Jolly);
+                        }
+                        assert(validComb.isValidCombination()); validCombinations.push_back(validComb);
+                        validComb.clear();
+
+                        last=-1;
+                        firstValid =-1;
+                        numPiecesTofirst=numPiecesToLast;
+                    }
+                    else
+                    {
+                        last=-1;
+                        firstValid =-1;
+                        numPiecesTofirst=numPiecesToLast;
+                    }
+
+                }
+                else if(counter[j]>0)
+                {
+                    if(firstValid==-1)
+                    {
+                        numPiecesTofirst+=counter[j];
+                        firstValid=j;
+                    }
+                    else{ last=j;}
+                    numPiecesToLast+=counter[j];
                 }
 
-                last=-1;
-                firstValid =-1;
-                numPiecesTofirst=numPiecesToLast;
-            }
-            else if(counter[j]>0)
-            {
-                if(firstValid==-1)
-                {
-                    numPiecesTofirst+=counter[j];
-                    firstValid=j;
-                }
-                else{ last=j;}
-                numPiecesToLast+=counter[j];
-            }
         }
     }
 }
